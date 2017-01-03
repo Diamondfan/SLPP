c4d8f6a5796991e621208f0d1fb05bce04ad4fc7
Richardfan <fanruchao@bupt.edu.cn>
2017-01-03 22:08:37 +0800

    add a new supervised learning function

--- LPP.py
+++ LPP.py
@@ -9,13 +9,10 @@ def calulateDistance(data,frame_num):
     D=np.zeros((frame_num,frame_num),dtype=float)
     beta=0
     for i in range(frame_num):
-        for j in range(i,frame_num):
+        for j in range(i+1,frame_num):
             D[i,j]=np.linalg.norm(data[:,j]-data[:,i])
             D[j,i]=D[i,j]
-            if i==j:
-                beta+=D[i,j]
-            else:
-                beta=beta+2*D[i,j]
+            beta=beta+2*D[i,j]
     beta=(beta/(frame_num**2))**2
     print("beta="+str(beta))
     #print("max="+str(max))
@@ -28,9 +25,9 @@ def supervisedLearningA(D,beta,frame_num,k_hood,label):
     for i in range(frame_num):
         for j in range(i,frame_num):
             if label[i]==label[j]:
-                D[i,j]=math.sqrt(math.exp(-D[i,j]**2/beta))
-            else:
                 D[i,j]=math.sqrt(1-math.exp(-D[i,j]**2/beta))
+            else:
+                D[i,j]=math.sqrt(math.exp(-D[i,j]**2/beta))
             D[j,i]=D[i,j]
             #sum_cal_t = sum_cal_t+D[i,j]+D[j,i]
     
@@ -51,6 +48,27 @@ def supervisedLearningA(D,beta,frame_num,k_hood,label):
     return D
     #print(D)
 
+def newLearningA(D,beta,frame_num,k_hood,label):    
+    #sum_cal_t=0
+ 
+    for i in range(frame_num):
+        dis=list()
+        for j in range(frame_num):
+            dis.append(D[i,j])
+        dis.sort(reverse=False)
+        for j in range(frame_num):
+            if D[i,j] >= dis[k_hood+1]:
+                D[i,j]=0
+            elif label[i]==label[j]:
+                D[i,j]=math.sqrt(math.exp(-D[i,j]**2/beta))
+            else:
+                D[i,j]=math.sqrt(1-math.exp(-D[i,j]**2/beta))
+    for i in range(frame_num):
+        for j in range(frame_num):
+            if D[i,j]==0:
+                D[i,j]=D[j,i]
+    
+    return D
 
 def supervisedLearningB(D,beta,frame_num,k_hood,label):    
     max_same=0
